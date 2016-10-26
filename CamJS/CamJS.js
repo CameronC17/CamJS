92176a55c183a687c7be93e8b4fceaec39b105c1
CameronC17 <cameronchalmers@hotmail.co.uk>
2016-10-26 14:16:24 +0100

    tiles drawing, player on the tiles

--- CamJS/CamJS.js
+++ CamJS/CamJS.js
@@ -3,30 +3,172 @@
 //classes
 class Tile {
 	constructor() {
-		this.tileSize = {"width": 0, "height": 0};
+		this.tiles = [];
+		this.tileSize = [10, 10];
+		this.mapSize = [0, 0];
+		this.playerOffset = [0, 0];
+		this.moveLimit = 0;
 		console.log("Tile game loaded!");
 	}
 
 	set(settings) {
-		this.tileSize = settings.tileSize;
+		if (settings.tiles)
+			this.tiles = settings.tiles;
+		if (settings.tileSize)
+			this.tileSize = settings.tileSize;
+		if (settings.playerOffset)
+			this.playerOffset = settings.playerOffset;
+		if (settings.moveLimit)
+			this.moveLimit = settings.moveLimit;
+
+		if (settings.tiles && settings.tileSize) {
+			this.mapSize[0] = settings.tiles[0].length * settings.tileSize[0];
+			this.mapSize[1] = settings.tiles.length * settings.tileSize[1];
+		}
+	}
+
+	createPlayer(player) {
+		var plyr = {
+			"name": player.name,
+			"position": player.position,
+			"size": player.size,
+			"offset": [0, 0]
+		}
+		return plyr;
+	}
+
+	engine(players) {
+
+	}
+
+	drawTile(ctx, x, y, tile) {
+		ctx.fillStyle="#000";
+		switch (tile) {
+			case "1":
+				ctx.fillStyle="#f3f355";
+				break;
+			case "2":
+				ctx.fillStyle="#05f7f8";
+				break;
+			case "3":
+				ctx.fillStyle="#ff6600";
+				break;
+			case "4":
+				ctx.fillStyle="#333333";
+				break;
+			case "5":
+				ctx.fillStyle="#f080f0";
+				break;
+			case "6":
+				ctx.fillStyle="#33ff33";
+				break;
+			case "7":
+				ctx.fillStyle="#ff2200";
+				break;
+		}
+		ctx.fillRect(x, y, this.tileSize[0],this.tileSize[1]);
+	}
+
+	drawLand(c, ctx, camera) {
+		var drawX = camera.anchor[0] - (this.mapSize[0]/2);
+		var drawY = camera.anchor[1] - (this.mapSize[1]/2);
+
+		for (var y = 0; y < this.tiles.length; y++) {
+			for (var x = 0; x < this.tiles[y].length; x++) {
+				this.drawTile(ctx, drawX, drawY, this.tiles[y][x]);
+				drawX += this.tileSize[0];
+			}
+			drawX = camera.anchor[0] - (this.mapSize[0]/2);
+			drawY += this.tileSize[1];
+		}
+	}
+
+	drawPlayers(c, ctx, camera, players) {
+		var drawX = camera.anchor[0] - (this.mapSize[0]/2);
+		var drawY = camera.anchor[1] - (this.mapSize[1]/2);
+
+		for (let player of players) {
+			ctx.fillStyle="#ee33ff";
+			var playerX = drawX + (player.position[0] * this.tileSize[0]) + this.playerOffset[0],
+			 		playerY = drawY + (player.position[1] * this.tileSize[1]) + this.playerOffset[1];
+			ctx.fillRect(playerX, playerY, player.size[0], player.size[1]);
+		}
+	}
+
+	draw(c, ctx, camera, players) {
+		this.drawLand(c, ctx, camera)
+		this.drawPlayers(c, ctx, camera, players)
+		//this.drawUI(c, ctx)
 	}
 }
 
 
+/**
+CamJS Engine. Copyright 2016 Cameron Chalmers
 
-
-//actual CamJS game engine!
+@params "canvas": string reference to the name of the canvas on the page
+@params "gamemode": string denoting the type of game mode to be played.
+*/
 class CamJS {
-	constructor(canvas, gametype) {
+	constructor(canvas, gamemode) {
+		console.log("CamJS starting...");
 		this.c = document.getElementById(canvas);
 		this.ctx = this.c.getContext("2d");
 		//0 - mouse down, 1 - mouse up, 2 - mouse move
 		this.mousePos = [{"x": -1, "y": -1}, {"x": -1, "y": -1}, {"x": -1, "y": -1}];
-		this.type = this.getGameType(gametype);
+		this.players = [];
+		this.camera = {
+			"type": null,
+			"anchor": [0, 0]
+		};
+		this.Key = this.createKeyObject();
+		this.mode = this.getGameMode(gamemode);
+		this.controls = null;
+		console.log("CamJS loaded.");
 	}
 
-	getGameType(type) {
-		switch (type) {
+	/**
+	createKeyObject will return a JSON object that is used to store information about key presses
+	*/
+	createKeyObject() {
+		var rtn = {
+			_pressed: {},
+		  LEFT: 65,
+		  UP: 87,
+		  RIGHT: 68,
+		  DOWN: 83,
+		  isDown: function(keyCode) {
+		    return this._pressed[keyCode];
+		  }
+		}
+		return rtn;
+	}
+
+	/**
+	set accepts settings and changes the game modes based on the user input
+
+	@params "settings": a json object that holds information about the game settings
+	*/
+	set(settings) {
+		this.camera.type = settings.camera;
+		if (settings.camera == "follow") {
+			let cameraX = this.c.width / 2;
+			let cameraY = this.c.height / 2;
+			this.camera.anchor = [cameraX, cameraY];
+		} else if (settings.camera == "static") {
+			let cameraX = 0;
+			let cameraY = 0;
+			this.camera.anchor = [cameraX, cameraY];
+		}
+	}
+
+	/**
+	getGameMode sets the camJS class game mode to whatever the user requests
+
+	@params "mode": a string that holds information as to what type of game
+	*/
+	getGameMode(mode) {
+		switch (mode) {
 			case "tile":
 				return new Tile();
 			default:
@@ -35,7 +177,21 @@ class CamJS {
 		}
 	}
 
-	//mouse stuff 
+	addPlayer(player) {
+		var plyr = this.mode.createPlayer(player);
+		this.players.push(plyr);
+	}
+
+	//keyboard stuff
+	keyUp(event) {
+		delete this.Key._pressed[event.keyCode];
+	}
+
+	keyDown(event) {
+		this.Key._pressed[event.keyCode] = true;
+	}
+
+	//mouse stuff
 	mouseDown(e) {
 		var pos = this.getMousePos(e);
 		this.mousePos[0] = pos;
@@ -61,8 +217,8 @@ class CamJS {
 	}
 
 	//main function that draws the game
-	draw() {
-		this.ctx.fillStyle="#000";
- 		this.ctx.fillRect(50, 50, 100, 100);
+	run() {
+		this.mode.engine(this.players);
+		this.mode.draw(this.c, this.ctx, this.camera, this.players);
 	}
 }
--- examples/js/example1.js
+++ examples/js/example1.js
@@ -1,36 +1,43 @@
 var animFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame    || window.oRequestAnimationFrame      || window.msRequestAnimationFrame     || null ;
 
-window.addEventListener('mousedown', mouseDown, false);
-window.addEventListener('mouseup', mouseUp, false);
-window.addEventListener('mousemove', mouseMove, false);
-
 //create a new instance of camJS, using the tile game mode
 var game = new CamJS("canvas", "tile");
 
-game.type.set({
-	"tileSize" : {"width": 40, "height": 40},
-	""
+//mouse listeners
+window.addEventListener('mousedown', function(event) { game.mouseDown(event); }, false);
+window.addEventListener('mouseup', function(event) { game.mouseUp(event); }, false);
+window.addEventListener('mousemove', function(event) { game.mouseMove(event); }, false);
 
+//key listeners
+window.addEventListener('keyup', function(event) { game.keyUp(event); }, false);
+window.addEventListener('keydown', function(event) { game.keyDown(event); }, false);
+
+game.set({
+	"camera": "follow",
+	"controls": "wasd"
 })
 
+game.addPlayer({
+	"name": "Cameron",
+	"position": [3, 5],
+	"size": [30, 60]
+})
+
+game.mode.set({
+	"tiles": 	[ ["1","2","3","4","5","6","7"],
+							["2","3","6","7","4","2","1"],
+							["4","5","1","2","3","5","1"],
+						  ["3","2","5","3","7","5","6"],
+						  ["6","2","3","7","2","3","4"]  ],
+	"tileSize": [50, 50],
+	"playerOffset": [10, -70],
+	"moveLimit": 100
+});
 
 
 //now the animation part
 var recursiveAnim = function() {
-          game.draw();
+          game.run();
           animFrame(recursiveAnim);
     };
 animFrame(recursiveAnim);
-
-// MouseDown
-function mouseDown(e) {
-  game.mouseDown(e);
-}
-// MouseUp
-function mouseUp(e) {
-  game.mouseUp(e);
-}
-// MouseMove
-function mouseMove(e) {
-  game.mouseMove(e);
-}
